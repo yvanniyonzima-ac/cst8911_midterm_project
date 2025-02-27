51127f8b36a1fc64ce27ec81824e25c033005479
yvanniyonzima <yvannshuti@gmail.com>
2025-02-26 23:34:57 -0500

    - Added SQL querries used to create aggregation tables and insert data. - Added CSSV file with the Supermarket Sales Data

--- sql_querries.sql
+++ sql_querries.sql
@@ -20,3 +20,58 @@ SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'SalesData';
 
 -- Verify that teh SQL table contains the data from the csv file. Output in Step 13 in the report
 SELECT * FROM [dbo].[SalesData]
+
+-- State Sales Table 
+CREATE TABLE StateSales (
+    StateID INT IDENTITY PRIMARY KEY,  -- Unique ID for each region
+    State NVARCHAR(50) UNIQUE,         -- Region name (ensures no duplicates)
+    TotalStateSales DECIMAL(18,2) DEFAULT 0  -- Stores total sales for each region
+);
+
+INSERT INTO StateSales (State, TotalStateSales)
+SELECT 
+    s.State, 
+    SUM(s.Sales) AS TotalStateSales
+FROM SalesData s
+GROUP BY s.State;
+
+
+-- Consumer Sales Pie Table 
+CREATE TABLE dbo.consumersalespie ( 
+    Id INT NOT NULL PRIMARY KEY, 
+    Category VARCHAR(255) NULL, 
+    TotalSales INT NOT NULL 
+);
+
+INSERT INTO dbo.consumersalespie (Category, TotalSales) 
+SELECT Category, SUM(Sales) AS TotalSales 
+FROM dbo.SalesData 
+GROUP BY Category; 
+
+-- Consumer Customer Count Table 
+CREATE TABLE ConsumerCustomerCount ( 
+    Region VARCHAR(255) NULL, 
+    State VARCHAR(255) NULL, 
+    Customer_Count INT NULL 
+);
+
+INSERT INTO dbo.ConsumerCustomerCount (Region, State, Customer_Count) 
+SELECT Region, State, COUNT(*) AS Customer_Count 
+FROM dbo.SalesData 
+WHERE Segment = 'Consumer' 
+GROUP BY Region, State; 
+
+-- Customer Segment Profitability Table
+CREATE TABLE CustomerSegmentProfitability ( 
+    id INT NOT NULL PRIMARY KEY, 
+    segment VARCHAR(255) NULL, 
+    sales DECIMAL(18,2) NULL, 
+    profit DECIMAL(18,2) NULL 
+); 
+
+INSERT INTO dbo.CustomerSegmentProfitability (segment, sales, profit) 
+SELECT Segment, SUM(Sales) AS TotalSales, SUM(Profit) AS TotalProfit 
+FROM dbo.SalesData 
+WHERE Segment = 'Consumer' 
+GROUP BY Segment; 
+
